eaa1842a6304eb8d2dbe2bb90335e2b947ae6e27
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-04-29 09:59:58 -0600

    added my first test, it failed because I haven't made my guessing_game class yet

--- test/guessing_game_main_test.rb
+++ test/guessing_game_main_test.rb
@@ -3,6 +3,8 @@ require 'minitest/pride'
 require './lib/guessing_game'
 
 class GuessingGameTest < Minitest::Test
-
-
+  def test_that_the_game_exists
+    new_game = GuessingGame.new
+    assert_instance_of Guessing_game, new_game
+  end
 end
