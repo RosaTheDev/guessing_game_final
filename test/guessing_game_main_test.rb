1437f845855ab6a3bd96e901f65959c66e535616
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-04-29 10:02:01 -0600

    Turns out the test was failing because I tried to assert the instance of Guessing_game instead of GuessingGame, I fixed it

--- test/guessing_game_main_test.rb
+++ test/guessing_game_main_test.rb
@@ -5,6 +5,6 @@ require './lib/guessing_game'
 class GuessingGameTest < Minitest::Test
   def test_that_the_game_exists
     new_game = GuessingGame.new
-    assert_instance_of Guessing_game, new_game
+    assert_instance_of GuessingGame, new_game
   end
 end
