a2145d9233ec60d3775f91873774312ac59da31a
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-04-29 19:25:05 -0600

    changed new_game.pre_game to new_game.(instance variable method name)

--- test/guessing_game_main_test.rb
+++ test/guessing_game_main_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/guessing_game'
+require './lib/game_text_module'
 
 class GuessingGameTest < Minitest::Test
   def test_that_the_game_exists
@@ -10,7 +11,7 @@ class GuessingGameTest < Minitest::Test
 
   def test_that_the_game_generates_a_random_number
     new_game = GuessingGame.new
-    assert new_game.pre_game.between?(1,100)
+    assert new_game.random_number.between?(1,100)
   end
 
   def test_that_the_wrong_guess_is_defaulted_to_true
