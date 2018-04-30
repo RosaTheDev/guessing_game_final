8b58343190ad8c6c08a42af83cbb9c89775ae91b
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-04-29 19:17:59 -0600

    Addded a few modules to print out the right answer text, the guess is low text, and the guess is too high text, and also a string interpolation for the cheating method

---
+++ lib/game_text_module.rb
@@ -0,0 +1,20 @@
+module GameText
+  def starting_game_text
+    puts "I have generated a random number for you to guess, what is your guess?\n>"
+  end
+
+  def right_answer
+    puts "You guessed the right number!"
+  end
+
+  def answer_too_low
+    puts "Too bad its too low!"
+  end
+  def answer_too_high
+    puts "Oh no! its too high!"
+  end
+
+  def prints_the_random_number
+    puts "#{@random_number} hehehehehe you suck.."
+  end
+end
