ed01a3c9802947c213101b9808f9e42d94635fd3
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-04-29 19:25:27 -0600

    changed the first method name from pre_game to initialize

--- lib/guessing_game.rb
+++ lib/guessing_game.rb
@@ -1,10 +1,11 @@
 require_relative 'game_text_module'
 class GuessingGame
   include GameText
-  attr_reader :random_number
-  def pre_game
+  attr_reader :random_number, :wrong_guess
+  def initialize
     starting_game_text
     @random_number = rand(1...100)
+    @wrong_guess = true
   end
 
 
